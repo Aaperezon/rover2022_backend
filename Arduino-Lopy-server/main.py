3ab2f3b5b44e9d319b401970fbcf53d5edec9351
Aaron <aaron-po@hotmail.co.uk>
2022-04-12 17:28:59 -0500

    test1

--- Arduino-Lopy-server/main.py
+++ Arduino-Lopy-server/main.py
@@ -2,36 +2,19 @@ import serial
 import time
 import requests
 
+
 lopy = serial.Serial(port='/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_DO0037UU-if00-port0', baudrate=9600)
 arduino = serial.Serial(port='/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_758303338373514130F0-if00', baudrate=9600)
-def getPayload(message):
-    message = message.split()
-    variables = {
-        "heart_rate1": message[0], 
-        "heart_rate2": message[1],
-        "temperature": message[2],
-        "humidity":    message[3],
-        "speed":       message[4],
-        "roll":        message[5],
-        "pitch":       message[6],
-        "yaw":         message[7],
-        "latitude":    message[8],
-        "longitude":   message[9]
-    }
-    # for i,key in enumerate(variables.keys()):
-    #     variables[key] = message[i]
-    return variables     
+
         
 def run():
     global lopy, arduino
     try:
         message = (arduino.readline()).decode('utf-8')
         lopy.write(message.encode('utf-8'))
-        print( message)
-        # print((lopy.readline()).decode('utf-8'))
-        # payload = getPayload(message)
-        #requests.post("http://localhost:81/rover2022/service/createsensor.php", json=payload, timeout=.1)
-        time.sleep(.05)
+        # print( message)
+        requests.get("http://localhost:81/rover2022/service/createsensor.php", json=payload, timeout=.1)
+        
     except KeyboardInterrupt:
         exit()
     except:
--- Lopy-station/main.py
+++ Lopy-station/main.py
@@ -1,41 +1,83 @@
 import serial 
 import time
 import requests
-
-lopy = serial.Serial(port='COM8', baudrate=115200)
-  
+import random
+from decimal import Decimal
+try:
+    lopy = serial.Serial(port='COM8', baudrate=115200)
+except:
+    pass  
 def getPayLoad(message):  
     message = message.split()
     variables = {
-        "vibration1": message[0], 
-        "vibration2": message[1],
-        "vibration3": message[2],
-        "vibration4":    message[3],
-        "gasCO2":       message[4],
-        "humidity":        message[5],
-        "pressure":       message[6],
-        "altitude":         message[7],
-        "temperature":    message[8],
-        "weight1":    message[9],
-        "weight2":    message[10],
-        "gyrox":    message[11],
-        "gyroy":    message[12],
-        "gyroz":    message[13],
-        "encoder":    message[14],
+        "vibration1":         message[0], 
+        "vibration2":         message[1],
+        "vibration3":         message[2],
+        "vibration4":         message[3],
+        "gasCO2":       float(message[4]),
+        "humidity":     float(message[5]),
+        "pressure":     float(message[6]),
+        "altitude":     float(message[7]),
+        "temperature":  float(message[8]),
+        "roll":         float(message[9]),
+        "pitch":        float(message[10]),
+        "yaw":          float(message[11]),
+        "speed":       float( message[12]),
+       "gravity1_x":    round(random.uniform(15,18), N),
+        "gravity1_y":    round(random.uniform(15,18), N),
+        "gravity1_z":    round(random.uniform(15,18), N),
+        "light1":    round(random.uniform(15,18), N),
+        "heart_rate1":    float( random.uniform(100,120) ),
     }
     return variables
+def getPayLoadSim():  
+    N = 2
+    variables = {
+        "vibration1": "LOW", 
+        "vibration2": "MODERATE",
+        "vibration3":"HIGH",
+        "vibration4":   "NULL",
+        "gasCO2":      round(random.uniform(1,2), N),
+        "humidity":        round(random.uniform(20,100), N),
+        "pressure":       round(random.uniform(1,2), N),
+        "altitude":        round(random.uniform(1,2), N),
+        "temperature":   round(random.uniform(20,40), N),
+        "roll":    round(random.uniform(1,2), N),
+        "pitch":    round(random.uniform(1,2), N),
+        "yaw":    round(random.uniform(1,2), N),
+        "speed":    round(random.uniform(15,18), N),
+        "gravity1_x":    round(random.uniform(15,18), N),
+        "gravity1_y":    round(random.uniform(15,18), N),
+        "gravity1_z":    round(random.uniform(15,18), N),
+        "light1":    round(random.uniform(15,18), N),
+        "heart_rate1":    200
+    }
+    return variables
+
 def run():
+    global lopy
     try:
         message = (lopy.readline()).decode('utf-8')
+        payload = getPayLoad(message)
         print( message )
-        # payload = getPayload(message)
-        # requests.post("http://localhost:81/rover2022/service/createsensor.php", json=payload, timeout=.1)
+        requests.post("http://localhost:81/rover2022/service/createsensor.php", json=payload, timeout=.1)
     except Exception as e:
-        print(e)
+        # print(e)
+        if lopy.isOpen():
+                lopy.close()
+        else:
+            try:
+                lopy = serial.Serial(port='COM8', baudrate=115200)
+            except:
+                pass
    
-
+def simulate():
+        payload = getPayLoadSim()
+        print(payload)
+        requests.post("http://localhost:81/rover2022/service/createsensor.php", json=payload, timeout=.1)
+        time.sleep(.5)
 
 if __name__ == '__main__':
     while True:
         run()
-        # time.sleep(1)
+        # time.sleep(.05)
